bd0e9f85fecc4e458063cd29a2b9441491214c7b
kennck <kennettecanales@gmail.com>
2021-01-13 18:48:51 +0800

    New

--- src/router/dev_routes/app.js
+++ src/router/dev_routes/app.js
@@ -38,7 +38,7 @@ export default{
   }, {
     path: '/login',
     name: 'loginAccount',
-    component: resolve => require(['component/increment/basic/Login.vue'], resolve),
+    component: resolve => require(['components/increment/basic/LogIn.vue'], resolve),
     meta: {
       tokenRequired: false
     }
