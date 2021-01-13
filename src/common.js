78d76f24854e22afe2a6bcbbf74e9d619f1ce583
pathran-phantom1823 <patrick.pathran23@gmail.com>
2021-01-13 18:43:53 +0800

    frame enhance

--- src/common.js
+++ src/common.js
@@ -7,10 +7,7 @@ export default {
       {id: 2, users: 'ALL', parent_id: 0, description: 'CROCKERY PICK UP/ RETURN', icon: '', path: 'requests', flag: false, subMenu: null},
       {id: 3, users: 'ALL', parent_id: 0, description: 'DELI PRODUCTS', icon: '', path: 'investments', flag: false, subMenu: null},
       {id: 4, users: 'ALL', parent_id: 0, description: 'MENU ITEMS', icon: '', path: 'payments', flag: false, subMenu: null},
-      {id: 5, users: 'ALL', parent_id: 0, description: 'ANALIYTICS', icon: '', path: 'deposits', flag: false, subMenu: null},
-      {id: 6, users: 'ALL', parent_id: 0, description: '', icon: '', path: 'deposits', flag: false, subMenu: null},
-      {id: 7, users: 'ALL', parent_id: 0, description: 'DELIVERY', icon: 'fas fa-shipping-fast', path: 'withdrawals', flag: false, subMenu: null},
-      {id: 8, users: 'ALL', parent_id: 0, description: 'COLLECTION', icon: 'fas fa-hand-holding-usd', path: 'withdrawals', flag: false, subMenu: null}
+      {id: 5, users: 'ALL', parent_id: 0, description: 'ANALYTICS', icon: '', path: 'deposits', flag: false, subMenu: null}
   ],
   menuOff: [
       {id: 1, users: 'ALL', parent_id: 0, description: 'Dashboard', icon: 'fa fa-tachometer', path: 'dashboard', flag: false, subMenu: null},
